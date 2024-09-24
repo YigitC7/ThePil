a3a28b98007d5504a1ac14c22f9bc4133ff7e754
YigitC7 <yigitcitak007@gmail.com>
2024-09-24 15:35:19 +0300

    Add files via upload

---
+++ pil-start.c
@@ -0,0 +1,12 @@
+#include <stdlib.h>
+
+/*
+ * Pil başlatma programı
+ * Bu C programı derlenmiş pil programını çalıştırmaya yarıyor
+*/
+int main(){
+
+    system("/usr/local/bin/the-pil/pil");
+
+    return 0;
+}
