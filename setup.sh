5c7b9eed6dbe1d74eb5033062147f855a604f6d3
YigitC7 <yigitcitak007@gmail.com>
2024-09-24 15:49:34 +0300

    Add files via upload

--- setup.sh
+++ setup.sh
@@ -1,3 +1,12 @@
+#!/bin/bash
+
 sudo cp -r The-pil /usr/local/bin/the-pil
 sudo cp pil-start_derlenmis /usr/local/bin/pil
-echo baslatmak icin pil yazın
+
+sudo chmod +x /usr/local/bin/pil
+sudo chmod -R +x /usr/local/bin/the-pil
+
+sudo chown -R root:root /usr/local/bin/the-pil
+sudo chown root:root /usr/local/bin/pil
+
+echo "Başlatmak için 'pil' yazın."
